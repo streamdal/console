204d3f4441cb73e7eaff714b8531737873bf77bc
Adelle <apitsas4@gmail.com>
2023-10-12 11:18:52 -0400

    wire up delete service flow

---
+++ routes/service/[service]/delete.tsx
@@ -0,0 +1,49 @@
+import { Handlers } from "$fresh/src/server/types.ts";
+import { serviceSignal } from "../../../components/serviceMap/serviceSignal.ts";
+import { ResponseCode } from "streamdal-protos/protos/sp_common.ts";
+import { getAttachedPipeline } from "../../../lib/utils.ts";
+import { deleteAudience, detachPipeline } from "../../../lib/mutation.ts";
+
+export const handler: Handlers<> = {
+  async POST(req, ctx) {
+    const pipelines = serviceSignal.value.pipelines;
+    const config = serviceSignal.value.config;
+    const audiences = serviceSignal.value.audiences.filter((a) =>
+      a.serviceName === ctx.params.service
+    );
+    const responses = audiences.map(async (a) => {
+      const attachedPipeline = await getAttachedPipeline(
+        a,
+        pipelines,
+        config,
+      );
+      if (attachedPipeline) {
+        return await detachPipeline(attachedPipeline.id, a);
+      }
+      if (!attachedPipeline || response?.code === ResponseCode.OK) {
+        return await deleteAudience(a);
+      }
+    });
+
+    const response = await responses.find((r) => r.code !== ResponseCode.OK) ||
+      responses.find((r) => r.code === ResponseCode.OK);
+
+    return new Response(
+      JSON.stringify({
+        status: 307,
+        success: {
+          status: response.code === ResponseCode.OK,
+          message: response.code === ResponseCode.OK
+            ? "Successfully deleted"
+            : response.message,
+        },
+        headers: { Location: "/" },
+      }),
+      { status: response.code === ResponseCode.OK ? 200 : 400 },
+    );
+  },
+};
+
+export default function DeleteServiceRoute() {
+  return null;
+}
