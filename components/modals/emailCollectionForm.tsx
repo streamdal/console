02d8e9e206aaf4f32a0051e9ba0cc92747a0dfd4
Adelle <apitsas4@gmail.com>
2023-10-18 15:30:02 -0400

    fmt deno

--- components/modals/emailCollectionForm.tsx
+++ components/modals/emailCollectionForm.tsx
@@ -1,70 +1,68 @@
-import {useState} from "preact/hooks";
-import {FormInput} from "../form/formInput.tsx";
-import {validate} from "../form/validate.ts";
-import {zfd} from "zod-form-data";
+import { useState } from "preact/hooks";
+import { FormInput } from "../form/formInput.tsx";
+import { validate } from "../form/validate.ts";
+import { zfd } from "zod-form-data";
 import * as z from "zod/index.ts";
 
-
 export const EmailSchema = zfd.formData({
-    email: z.string().min(1, {message: "Required to submit"}),
+  email: z.string().min(1, { message: "Required to submit" }),
 });
 export const EmailCollectionForm = (
-    {verificationStatus}: { verificationStatus: any },
+  { verificationStatus }: { verificationStatus: any },
 ) => {
-    const [errors, setErrors] = useState<string>("");
-    const [data, setData] = useState("");
+  const [errors, setErrors] = useState<string>("");
+  const [data, setData] = useState("");
 
-    const customTheme: CustomFlowbiteTheme["button"] = {
-        color: {
-            primary: "bg:red-100 hover:bg-red-600",
-        },
-    };
+  const customTheme: CustomFlowbiteTheme["button"] = {
+    color: {
+      primary: "bg:red-100 hover:bg-red-600",
+    },
+  };
 
-    const onSumbit = async (e: any) => {
-        const emailData = new FormData(e.target);
-        const {errors} = validate(EmailSchema, emailData);
-        setErrors(errors || {});
+  const onSumbit = async (e: any) => {
+    const emailData = new FormData(e.target);
+    const { errors } = validate(EmailSchema, emailData);
+    setErrors(errors || {});
 
-        if (errors) {
-            e.preventDefault();
-            return;
-        }
-    };
+    if (errors) {
+      e.preventDefault();
+      return;
+    }
+  };
 
-    return (
-        <div
-            class="w-screen h-screen flex flex-col justify-center align-top items-center bg-login bg-cover bg-center  bg-no-repeat">
-            <form
-                class={"rounded-xl px-6 py-10 items-center bg-white w-[400px] rounded-xl px-6 py-10 items-center bg-white w-[400px]"}
-                action="/email/submit"
-                method="post"
-                onSubmit={() => onSumbit()}
-            >
-                <div className={"w-full items-center"}>
-                    <img
-                        src={"/images/logo.svg"}
-                        className={"w-16 ml-[144px] mb-4 max-w-16"}
-                    />
-                </div>
-                <h2 className={"text-center mb-3 text-3xl font-display tracking-wide"}>
-                    Please enter your email to continue
-                </h2>
-                <FormInput
-                    name="email"
-                    label="Email"
-                    errors={errors}
-                    data={data}
-                    setData={setData}
-                />
-                <div className={"flex flex-col items-center"}>
-                    <button
-                        type={"submit"}
-                        className={"bg-streamdalYellow btn-heimdal text-web mb-3 w-full font-bold"}
-                    >
-                        Get verification code
-                    </button>
-                </div>
-            </form>
+  return (
+    <div class="w-screen h-screen flex flex-col justify-center align-top items-center bg-login bg-cover bg-center  bg-no-repeat">
+      <form
+        class={"rounded-xl px-6 py-10 items-center bg-white w-[400px] rounded-xl px-6 py-10 items-center bg-white w-[400px]"}
+        action="/email/submit"
+        method="post"
+        onSubmit={() => onSumbit()}
+      >
+        <div className={"w-full items-center"}>
+          <img
+            src={"/images/logo.svg"}
+            className={"w-16 ml-[144px] mb-4 max-w-16"}
+          />
         </div>
-    );
+        <h2 className={"text-center mb-3 text-3xl font-display tracking-wide"}>
+          Please enter your email to continue
+        </h2>
+        <FormInput
+          name="email"
+          label="Email"
+          errors={errors}
+          data={data}
+          setData={setData}
+        />
+        <div className={"flex flex-col items-center"}>
+          <button
+            type={"submit"}
+            className={"bg-streamdalYellow btn-heimdal text-web mb-3 w-full font-bold"}
+          >
+            Get verification code
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
--- routes/email/submit.tsx
+++ routes/email/submit.tsx
@@ -1,37 +1,37 @@
-import {Handlers} from "$fresh/src/server/types.ts";
-import {sendEmail} from "../../lib/mutation.ts";
-import {ResponseCode} from "streamdal-protos/protos/sp_common.ts";
-import {AppRegistrationRequest} from "streamdal-protos/protos/sp_external.ts";
-import {ErrorType, validate} from "../../components/form/validate.ts";
-import {EmailSchema} from "../../components/modals/emailCollectionForm.tsx";
+import { Handlers } from "$fresh/src/server/types.ts";
+import { sendEmail } from "../../lib/mutation.ts";
+import { ResponseCode } from "streamdal-protos/protos/sp_common.ts";
+import { AppRegistrationRequest } from "streamdal-protos/protos/sp_external.ts";
+import { ErrorType, validate } from "../../components/form/validate.ts";
+import { EmailSchema } from "../../components/modals/emailCollectionForm.tsx";
 
 export const handler: Handlers<> = {
-    async POST(req, ctx) {
-        console.log(ctx.params);
-        const emailData = await req.formData();
-        const {data: email, errors}: {
-            email: AppRegistrationRequest;
-            errors: ErrorType
-        } = validate(EmailSchema, emailData)
-        const response = await sendEmail(email.email);
-        const {session} = ctx.state;
+  async POST(req, ctx) {
+    console.log(ctx.params);
+    const emailData = await req.formData();
+    const { data: email, errors }: {
+      email: AppRegistrationRequest;
+      errors: ErrorType;
+    } = validate(EmailSchema, emailData);
+    const response = await sendEmail(email.email);
+    const { session } = ctx.state;
 
-        return new Response(
-            JSON.stringify({
-                status: 307,
-                success: {
-                    status: response.code === ResponseCode.OK,
-                    message: response.code === ResponseCode.OK
-                        ? "Email successfully sent"
-                        : response.message,
-                },
-                headers: {Location: "/"},
-            }),
-            {status: response.code === ResponseCode.OK ? 200 : 400},
-        );
-    },
+    return new Response(
+      JSON.stringify({
+        status: 307,
+        success: {
+          status: response.code === ResponseCode.OK,
+          message: response.code === ResponseCode.OK
+            ? "Email successfully sent"
+            : response.message,
+        },
+        headers: { Location: "/" },
+      }),
+      { status: response.code === ResponseCode.OK ? 200 : 400 },
+    );
+  },
 };
 
 export default function submitEmailRoute() {
-    return null;
+  return null;
 }
