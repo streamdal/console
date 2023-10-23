df7d5f3f2f68ad96f1980937cd862d9f49bd02d8
Adelle <apitsas4@gmail.com>
2023-10-23 11:49:33 -0400

    add blank icon if no live info

--- islands/opModal.tsx
+++ islands/opModal.tsx
@@ -58,6 +58,17 @@ export default function OpModal(
         </div>
       );
     }
+    return (
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="48" height="48" rx="24" fill="#E6DDFE" />
+      </svg>
+    );
   };
 
   const audience = opModal.value?.audience;
