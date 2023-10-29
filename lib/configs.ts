7d8746b176e3cba7a6e8a7525aab8318cd9c6c94
Daniel Selans <daniel.selans@gmail.com>
2023-10-29 14:15:59 -0700

    some more port + README updates
    docker-compose port update; some env var updates; some README bits

--- lib/configs.ts
+++ lib/configs.ts
@@ -9,7 +9,7 @@ Deno.env.set(
   env["STREAMDAL_CONSOLE_SESSION_KEY"] ?? crypto.randomUUID(),
 );
 export const GRPC_URL = env["STREAMDAL_CONSOLE_GRPC_WEB_URL"] ??
-  Deno.env.get("STREAMDAL_CONSOLE_GRPC_WEB_URL") ?? "http://localhost:9091";
+  Deno.env.get("STREAMDAL_CONSOLE_GRPC_WEB_URL") ?? "http://localhost:8083";
 export const GRPC_TOKEN = env["STREAMDAL_CONSOLE_GRPC_AUTH_TOKEN"] ??
   Deno.env.get("STREAMDAL_CONSOLE_GRPC_AUTH_TOKEN");
 export const PRODUCTION = env["STREAMDAL_CONSOLE_PRODUCTION"] ??
